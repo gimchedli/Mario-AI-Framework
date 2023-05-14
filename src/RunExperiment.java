227e7dd2e4be327327ac89cf3bf43c0942002cbb
Giorgi Mchedlishvili <gimc@itu.dk>
2023-05-14 12:08:32 +0200

    deleted unused files

--- src/RunExperiment.java
+++ src/RunExperiment.java
@@ -15,22 +15,6 @@ import com.opencsv.CSVWriter;
 
 
 public class RunExperiment {
-    public static void printResults(MarioResult result) {
-        System.out.println("****************************************************************");
-        System.out.println("Game Status: " + result.getGameStatus().toString() +
-                " Percentage Completion: " + result.getCompletionPercentage());
-        System.out.println("Lives: " + result.getCurrentLives() + " Coins: " + result.getCurrentCoins() +
-                " Remaining Time: " + (int) Math.ceil(result.getRemainingTime() / 1000f));
-        System.out.println("Mario State: " + result.getMarioMode() +
-                " (Mushrooms: " + result.getNumCollectedMushrooms() + " Fire Flowers: " + result.getNumCollectedFireflower() + ")");
-        System.out.println("Total Kills: " + result.getKillsTotal() + " (Stomps: " + result.getKillsByStomp() +
-                " Fireballs: " + result.getKillsByFire() + " Shells: " + result.getKillsByShell() +
-                " Falls: " + result.getKillsByFall() + ")");
-        System.out.println("Bricks: " + result.getNumDestroyedBricks() + " Jumps: " + result.getNumJumps() +
-                " Max X Jump: " + result.getMaxXJump() + " Max Air Time: " + result.getMaxJumpAirTime());
-        System.out.println("****************************************************************");
-    }
-
 
     public static String[] writeDataLineByLine(File file, MarioResult result, String level, int marioType)
     {
@@ -38,7 +22,7 @@ public class RunExperiment {
         // specified by filepath
         String[] data = {level, String.valueOf(marioType), String.valueOf(result.getCompletionPercentage()), String.valueOf((int) Math.ceil(result.getRemainingTime() / 1000f)), 
             String.valueOf(result.getKillsTotal()), String.valueOf(result.aliveOrDead()), String.valueOf(result.getDistanceTraversed())};
-        if(result.aliveOrDead()==false){System.out.println("oopsie");}
+        //if(result.aliveOrDead()==false){System.out.println("oopsie");}
         return data;
     }
 
@@ -81,7 +65,6 @@ public class RunExperiment {
 
     public static MarioAgent getCurrentAgent(MarioAgent agent){
         return agent;
-
     }
 
     public static void main(String[] args) throws Exception{
@@ -89,45 +72,45 @@ public class RunExperiment {
         MarioGame game = new MarioGame();
 
         //You can choose the agent you want to test as well as maximum time per lvl and generated lvl types from here
-        MarioAgent currentAgent = new agents.GandP.Agent();
-        String generatedMapString = "ge";
-        int inputTime = 20;
+        MarioAgent currentAgent = new agents.robinBaumgarten.Agent();
+        String generatedMapString = "patternOccur";
+        int inputTime = 60;
 
         String str = String.valueOf(currentAgent);
         String[] arrOfStr = str.split("\\.", 3);       
-        String testingagent = arrOfStr[1] + "_new"; // this will be the name of the generated csv file that contains ressults
+        String testingagent = arrOfStr[1] + "_new_718+"; // this will be the name of the generated csv file that contains ressults
            
         // TODO: Figure out how to check if directory and file exsists
         
         File file = new File("output/full_runs/" + generatedMapString + "/" + Integer.toString(inputTime) + "/" + testingagent + ".csv");
 
-            // create FileWriter object with file as parameter
+        // create FileWriter object with file as parameter
         FileWriter outputfile = new FileWriter(file);
 
-            // create CSVWriter object filewriter object as parameter
+        // create CSVWriter object filewriter object as parameter
         CSVWriter writer = new CSVWriter(outputfile);
 
-            // adding header to csv
+        // adding header to csv
         String[] header = { "Level", "Mario Type", "Progress", "Time Left", "Kills", "Mario Alive", "Distance"};
             
         writer.writeNext(header);
 
         
+        // If you want to run experiment on original levels you'll have to change maximum i valu to 16 instead of 1001, 
+        // since there are only 15 original levels
 
-
-        for(int i = 1; i < 10; i++) {
-            String temp = "./levels/" + generatedMapString + "/lvl-" + Integer.toString(3) + ".txt" ;
-            
-            
+        for(int i = 722; i < 723; i++) {
+            String temp = "./levels/" + generatedMapString + "/lvl-" + Integer.toString(i) + ".txt" ;
             for(int j = 0; j < 3; j++){
  
-                writer.writeNext(writeDataLineByLine(file, game.runGame(currentAgent, getLevel(temp), inputTime, j, false), 
-                generatedMapString + "LVL-" + Integer.toString(3) + ".txt" , j));            
+                writer.writeNext(writeDataLineByLine(file, game.runGame(currentAgent, getLevel(temp), inputTime, j, false, 1000), 
+                generatedMapString + "LVL-" + Integer.toString(i) + ".txt" , j));            
             }
-            System.out.println("Completed level " + 3);
+            System.out.println("Completed level " + i);
             
            
-        } 
+        }
+
         writer.close();
 
         long endTime   = System.nanoTime();
@@ -135,5 +118,11 @@ public class RunExperiment {
 
         System.out.println(totalTime/1000000000);
     }
+
+
+    // Experiment runtime:      20s / 60s / 240s
+    // Sergei Polikarpov:       27s / 51s / 160s
+    // Robin Baumgarten:        6579s / 
+    // Andy Sloane:             12533s / 
     
 }
